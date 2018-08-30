bd388d24774215b8f52d415397c14ed101a219da
Emmett <eekrupczak@gmail.com>
2018-08-30 13:03:07 -0400

    added packages

--- sampleGAN.py
+++ sampleGAN.py
@@ -1,6 +1,22 @@
-#### Sample GAN on MNIST
+#### Sample GAN on MNIST using Keras
 #### Taken from https://skymind.ai/wiki/generative-adversarial-network-gan
 #### E. Krupczak - 30 Aug 2018
+
+from __future__ import print_function, division
+
+from keras.datasets import mnist
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout
+from keras.layers import BatchNormalization, Activation, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
+
+import matplotlib.pyplot as plt
+
+import sys
+
+import numpy as np
 
 
 class GAN():
